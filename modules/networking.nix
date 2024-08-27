e20aded7b149314a5fbb9694aa2cf4c077bcef5b
DreamingCodes <me@dreaming.codes>
2024-08-27 23:32:50 +0200

    fix chromecast

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,15 +2,24 @@
   networking.networkmanager.enable = true;
 
   networking.firewall = {
-    allowedTCPPortRanges = [{
-      from = 1714;
-      to = 1764;
-    } # KDE Connect
+    allowedTCPPortRanges = [
+        # KDE Connect
+        {
+        from = 1714;
+        to = 1764;
+        }
       ];
-    allowedUDPPortRanges = [{
-      from = 1714;
-      to = 1764;
-    } # KDE Connect
-      ];
+    allowedUDPPortRanges = [
+        # KDE Connect
+        {
+            from = 1714;
+            to = 1764;
+        }
+        # chromecast
+        {
+            from = 32768;
+            to = 60999;
+        }
+    ];
   };
 }
