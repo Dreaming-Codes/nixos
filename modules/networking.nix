98cd5c4f0e4bf9c6f5a3d7b0361b08a9a708ef35
DreamingCodes <me@dreaming.codes>
2024-09-08 20:50:41 +0200

    add port for chromecast

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,11 +2,29 @@
   networking.networkmanager.enable = true;
 
   networking.firewall = {
+<<<<<<< Updated upstream
     allowedTCPPortRanges = [
         # KDE Connect
         {
         from = 1714;
         to = 1764;
+=======
+    allowedTCPPortRanges = [{
+      from = 1714;
+      to = 1764;
+    } # KDE Connect
+      ];
+    allowedUDPPortRanges = [
+        # KDE Connect
+        {
+            from = 1714;
+            to = 1764;
+        },
+        # chromecast
+        {
+            from = 32768;
+            to = 60999;
+>>>>>>> Stashed changes
         }
       ];
     allowedUDPPortRanges = [
