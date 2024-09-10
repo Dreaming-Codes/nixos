e4e2c4e6f3e625e02cf4341c8b260cf0a2bcfde9
DreamingCodes <me@dreaming.codes>
2024-09-10 13:58:55 +0200

    add lact service

--- desktop.nix
+++ desktop.nix
@@ -1,6 +1,15 @@
-{ pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    # amd gpu utility
-    lact
-  ];
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs;
+    [
+      # amd gpu utility
+      lact
+    ];
+
+  systemd.services.lact = {
+    description = "AMDGPU Control Daemon";
+    after = [ "multi-user.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = { ExecStart = "${pkgs.lact}/bin/lact daemon"; };
+    enable = true;
+  };
 }
