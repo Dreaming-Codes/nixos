bf4921e471815b90b0deb2e84f8d3df60e678997
DreamingCodes <me@dreaming.codes>
2024-11-14 19:36:59 +0100

    optimize laptop graphics

--- laptop.nix
+++ laptop.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    nrfutil
-  ];
+  environment.systemPackages = with pkgs; [ nrfutil ];
   nixpkgs.config.segger-jlink.acceptLicense = true;
   services = {
     udev = {
       extraRules = ''
-       SUBSYSTEM=="tty", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="522a", ATTRS{serial}=="84014353616C81E9", GROUP="wireshark", MODE="0666"
+        SUBSYSTEM=="tty", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="522a", ATTRS{serial}=="84014353616C81E9", GROUP="wireshark", MODE="0666"
       '';
     };
   };
@@ -15,10 +13,11 @@
     package = pkgs.wireshark;
   };
 
-  users = {
-    users.dreamingcodes = {
-      extraGroups = [ "wireshark" ];
-    };
-  };
+  # Enable rocm support for the iGPU on the laptop
+  nixpkgs.config.rocmSupport = true;
+  # Enable cuda support for the dGPU on the laptop
+  nixpkgs.config.cudaSupport = true;
+
+  users = { users.dreamingcodes = { extraGroups = [ "wireshark" ]; }; };
 
 }
