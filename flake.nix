8fd019deb6c61ef8597fddf4c4f1b942cece0e16
DreamingCodes <me@dreaming.codes>
2024-11-20 19:49:47 +0100

    add chaces to flake
    Signed-off-by: DreamingCodes <me@dreaming.codes>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "NixOS system flake";
 
+  nixConfig = {
+    extra-substituters = [ "https://chaotic-nyx.cachix.org" ];
+    extra-trusted-public-keys = [
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    ];
+  };
+
   inputs = {
     # NixOS official package source, using the nixos-unstable branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
