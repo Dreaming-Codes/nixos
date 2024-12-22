f8fa9b3ea4c6bc3ca88c96a9e032910d91a9b78e
DreamingCodes <me@dreaming.codes>
2024-12-22 19:03:59 +0100

    add sunshine to desktop

--- desktop.nix
+++ desktop.nix
@@ -7,6 +7,15 @@
     looking-glass-client
   ];
 
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+
   # Additional kernel modules needed for virtualization
   boot.initrd.availableKernelModules = [
     "vfio_pci"
