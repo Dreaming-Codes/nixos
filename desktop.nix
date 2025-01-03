91e2c3c65280eb779d0b014463298770cebcb3a8
DreamingCodes <me@dreaming.codes>
2025-01-03 14:57:11 +0100

    Revert "add waydroid and sunshine"
    This reverts commit 81009fb510c1aa31212c58676d01119dcf3a4d06.

--- desktop.nix
+++ desktop.nix
@@ -1,51 +1,11 @@
-{ pkgs, ... }:
-let
-  steam-run-url = pkgs.writeShellApplication {
-    name = "steam-run-url";
-    text = ''
-      echo "$1" > "/run/user/$(id --user)/steam-run-url.fifo"
-    '';
-    runtimeInputs = [
-      pkgs.coreutils # For `id` command
-    ];
-  };
-in {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     # amd gpu utility
     lact
     # amd encoders/decoders
     amf
     looking-glass-client
-    steam-run-url
-    xorg.libxcvt
   ];
-
-  systemd.user.services.steam-run-url-service = {
-    enable = true;
-    description = "Listen and starts steam games by id";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig.Restart = "on-failure";
-    script = toString (pkgs.writers.writePython3 "steam-run-url-service" { } ''
-      import os
-      from pathlib import Path
-      import subprocess
-
-      pipe_path = Path(f'/run/user/{os.getuid()}/steam-run-url.fifo')
-      try:
-          pipe_path.parent.mkdir(parents=True, exist_ok=True)
-          pipe_path.unlink(missing_ok=True)
-          os.mkfifo(pipe_path, 0o600)
-          while True:
-              with pipe_path.open(encoding='utf-8') as pipe:
-                  subprocess.Popen(['steam', pipe.read().strip()])
-      finally:
-          pipe_path.unlink(missing_ok=True)
-    '');
-    path = [ pkgs.steam ];
-  };
 
   services.sunshine = {
     enable = true;
@@ -53,11 +13,8 @@ in {
     capSysAdmin = true;
     openFirewall = true;
   };
-  systemd.user.services.sunshine.path = [ steam-run-url ];
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
-
-  virtualisation.waydroid.enable = true;
 
   # Additional kernel modules needed for virtualization
   boot.initrd.availableKernelModules = [
