fe0db779efb521eecf70eb6c2e188a4a5ea4bc8e
DreamingCodes <me@dreaming.codes>
2025-01-13 21:06:36 +0100

    add amf encoder

--- desktop.nix
+++ desktop.nix
@@ -37,7 +37,7 @@
 
     enable32Bit = true;
 
-    extraPackages = with pkgs; [ rocmPackages.clr.icd amdvlk ];
+    extraPackages = with pkgs; [ rocmPackages.clr.icd amdvlk amf ];
     extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
 
