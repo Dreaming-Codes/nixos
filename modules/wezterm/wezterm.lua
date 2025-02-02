c7601e8d1cc48d45741c8ede3cf8b22ef7a9e2cb
DreamingCodes <me@dreaming.codes>
2025-02-02 13:04:48 +0100

    remove alacritty

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -2,8 +2,17 @@
 local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+-- This is where you actually apply your config choices
+-- For example, changing the color scheme:
+config.color_scheme = 'Abernathy'
 -- Others
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "NONE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 -- and finally, return the configuration to wezterm
 return config
