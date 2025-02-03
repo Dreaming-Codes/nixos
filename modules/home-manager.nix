42ce03eaf19ebdbb200919a1219896d2388d2720
DreamingCodes <me@dreaming.codes>
2025-02-03 13:05:54 +0100

    add anyrun

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -9,6 +9,10 @@
     home.stateVersion = "24.11";
     programs.home-manager.enable = true;
 
+    imports = [
+      inputs.anyrun.homeManagerModules.default
+    ];
+
     # Hint Electron apps to use Wayland:
     home.sessionVariables = {
       EDITOR = "hx";
@@ -56,6 +60,8 @@
             "$mod, SPACE, exec, wezterm"
             ", Print, exec, ${pkgs.hyprshot}/bin/hyprshot -m active"
             "SHIFT, Print, exec, ${pkgs.hyprshot}/bin/hyprshot -m region"
+            "$mod, X, exec, anyrun"
+            "$mod, Q, killactive"
           ]
           ++ (
             # workspaces
@@ -124,6 +130,93 @@
     };
 
     programs = {
+      anyrun = {
+        enable = true;
+        extraConfigFiles = {
+          "uwsm_app.ron".text = ''
+            Config(
+              desktop_actions: false,
+              max_entries: 5,
+            )
+          '';
+
+          "shell.ron".text = ''
+            Config(
+              prefix: ">"
+            )
+          '';
+
+          "randr.ron".text = ''
+            Config(
+              prefi: ":dp",
+              max_entries: 5,
+            )
+          '';
+        };
+        extraCss = ''
+          * {
+            all: unset;
+            font-size: 1.2rem;
+          }
+
+          #window,
+          #match,
+          #entry,
+          #plugin,
+          #main {
+            background: transparent;
+          }
+
+          #match.activatable {
+            border-radius: 8px;
+            margin: 4px 0;
+            padding: 4px;
+            /* transition: 100ms ease-out; */
+          }
+          #match.activatable:first-child {
+            margin-top: 12px;
+          }
+          #match.activatable:last-child {
+            margin-bottom: 0;
+          }
+
+          #match:hover {
+            background: rgba(255, 255, 255, 0.05);
+          }
+          #match:selected {
+            background: rgba(255, 255, 255, 0.1);
+          }
+
+          #entry {
+            background: rgba(255, 255, 255, 0.05);
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            border-radius: 8px;
+            padding: 4px 8px;
+          }
+
+          box#main {
+            background: rgba(0, 0, 0, 0.5);
+            box-shadow:
+              inset 0 0 0 1px rgba(255, 255, 255, 0.1),
+              0 30px 30px 15px rgba(0, 0, 0, 0.5);
+            border-radius: 20px;
+            padding: 12px;
+          }
+        '';
+        config = {
+          width.fraction = 0.25;
+          y.fraction = 0.3;
+          hidePluginInfo = true;
+          closeOnClick = true;
+          plugins = with inputs.anyrun.packages.${pkgs.system}; [
+            inputs.anyrunfufexan.packages.${pkgs.system}.uwsm_app
+            randr
+            rink
+            shell
+            symbols
+          ];
+        };
+      };
       zellij = {
         enable = true;
         enableFishIntegration = true;
