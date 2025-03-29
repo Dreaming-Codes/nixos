9bfccc62ae78af8447f0510dbe5c5db635172a02
DreamingCodes <me@dreaming.codes>
2025-03-29 19:06:51 +0100

    transparent terminal

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
 config.color_scheme = 'Abernathy'
+config.window_background_opacity = 0.45
 -- Others
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
