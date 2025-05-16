7f4a8a1651f9a64f7c9e2b77ff0d9cc5540fb20f
DreamingCodes <me@dreaming.codes>
2025-05-16 11:54:41 +0200

    update qemu patch

--- modules/virtualization/qemu/pkg.nix
+++ modules/virtualization/qemu/pkg.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: let
   hypervisor-phantom_intel = {
     main = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/Scrut1ny/Hypervisor-Phantom/refs/heads/main/Hypervisor-Phantom/patches/QEMU/intel-qemu-9.2.0.patch";
+      url = "https://raw.githubusercontent.com/Scrut1ny/Hypervisor-Phantom/refs/heads/main/Hypervisor-Phantom/patches/QEMU/intel-qemu-9.2.3.patch";
       hash = "sha256-Rs4piJaLCdUO/iQuzgzOKwp2fOU8QtLQgJx6Y6KazIg=";
     };
     libnfs6 = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/Scrut1ny/Hypervisor-Phantom/refs/heads/main/Hypervisor-Phantom/patches/QEMU/qemu-9.2.0-libnfs6.patch";
+      url = "https://raw.githubusercontent.com/Scrut1ny/Hypervisor-Phantom/refs/heads/main/Hypervisor-Phantom/patches/QEMU/qemu-9.2.3-libnfs6.patch";
       hash = "sha256-HjZbgwWf7oOyvhJ4WKFQ996e9+3nVAjTPSzJfyTdF+4=";
     };
     cpu = builtins.readFile ./cpu.patch;
