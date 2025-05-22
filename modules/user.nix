d72b74a5075ecf2583a62875e2b93c4ac74c80c6
DreamingCodes <me@dreaming.codes>
2025-05-22 14:48:09 +0200

    add plugudev

--- modules/user.nix
+++ modules/user.nix
@@ -6,7 +6,7 @@
   users.users.dreamingcodes = {
     isNormalUser = true;
     description = "DreamingCodes";
-    extraGroups = ["networkmanager" "wheel" "docker" "libvirtd" "kvm" "adbusers" "input"];
+    extraGroups = ["networkmanager" "wheel" "docker" "libvirtd" "kvm" "adbusers" "input" "plugdev"];
     shell = pkgs.nushell;
   };
 }
