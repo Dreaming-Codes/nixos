0dccdf02975499857bb419a5c846a4baa3bb7d24
DreamingCodes <me@dreaming.codes>
2025-08-28 10:50:30 +0200

    comment secrstes

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   imports = [
-    /etc/nixos/secrets/github.nix
+    # /etc/nixos/secrets/github.nix
     ./modules/networking.nix
     # ./modules/davinci-resolve.nix
     ./modules/services.nix
