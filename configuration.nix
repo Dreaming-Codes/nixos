8784fe6ad07008e232d3233ce526abdfd657c2f1
DreamingCodes <me@dreaming.codes>
2025-08-28 10:57:11 +0200

    make build owkr on pure builds

--- configuration.nix
+++ configuration.nix
@@ -4,22 +4,27 @@
   inputs,
   ...
 }: {
-  imports = [
-    # /etc/nixos/secrets/github.nix
-    ./modules/networking.nix
-    # ./modules/davinci-resolve.nix
-    ./modules/services.nix
-    ./modules/system.nix
-    ./modules/user.nix
-    ./modules/home-manager.nix
-    ./modules/systemPrograms.nix
-    ./misc/sdks.nix
-    ./modules/steam.nix
-    ./modules/optimization.nix
-    ./modules/samba.nix
-    ./modules/nix.nix
-    ./modules/locales.nix
-    ./modules/hardware.nix
-    ./modules/boot.nix
-  ];
+  imports =
+    [
+      ./modules/networking.nix
+      # ./modules/davinci-resolve.nix
+      ./modules/services.nix
+      ./modules/system.nix
+      ./modules/user.nix
+      ./modules/home-manager.nix
+      ./modules/systemPrograms.nix
+      ./misc/sdks.nix
+      ./modules/steam.nix
+      ./modules/optimization.nix
+      ./modules/samba.nix
+      ./modules/nix.nix
+      ./modules/locales.nix
+      ./modules/hardware.nix
+      ./modules/boot.nix
+    ]
+    ++ (
+      if builtins.pathExists /etc/nixos/secrets/github.nix
+      then [/etc/nixos/secrets/github.nix]
+      else []
+    );
 }
