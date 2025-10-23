56a6fe7fada9ff05841806f550f08bd229cf87e5
DreamingCodes <me@dreaming.codes>
2025-10-23 13:51:43 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   nixConfig = {
     extra-substituters = [
       "https://chaotic-nyx.cachix.org"
-      "https://helix.cachix.org"
       "https://zed.cachix.org"
       "https://cache.garnix.io"
       "https://numtide.cachix.org"
     ];
     extra-trusted-public-keys = [
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
@@ -50,9 +48,6 @@
     razer-laptop-controller = {
       url = "github:JosuGZ/razer-laptop-control-no-dkms";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    helix = {
-      url = "github:helix-editor/helix";
     };
     nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
     vaultix.url = "github:milieuim/vaultix";
