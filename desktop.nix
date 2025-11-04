8460b64f0906954bab22cc515eb64f35dd225d03
DreamingCodes <me@dreaming.codes>
2025-11-03 18:59:54 -0800

    remove amdvlk due to deprecation

--- desktop.nix
+++ desktop.nix
@@ -102,8 +102,7 @@
 
     enable32Bit = true;
 
-    extraPackages = with pkgs; [rocmPackages.clr.icd amdvlk amf];
-    extraPackages32 = with pkgs; [driversi686Linux.amdvlk];
+    extraPackages = with pkgs; [rocmPackages.clr.icd amf];
   };
 
   environment.variables = {
