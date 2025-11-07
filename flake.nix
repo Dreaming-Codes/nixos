826a3facc52df92e9cabfe26198697d43df9c3c7
DreamingCodes <me@dreaming.codes>
2025-11-06 17:28:34 -0800

    add nix-community cache

--- flake.nix
+++ flake.nix
@@ -6,10 +6,12 @@
       "https://chaotic-nyx.cachix.org"
       "https://cache.garnix.io"
       "https://numtide.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
