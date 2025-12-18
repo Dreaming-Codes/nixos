d9bf20cc178fcac3875b0e71acc6fcd538dbddc0
DreamingCodes <me@dreaming.codes>
2025-12-17 18:43:55 -0800

    flatpak user auto update

--- desktop.nix
+++ desktop.nix
@@ -30,12 +30,31 @@
   services.flatpak.enable = true;
   services.packagekit.enable = true;
 
-  # Flatpak auto-update
+  # Flatpak auto-update (system-wide)
   systemd.services.flatpak-update = {
-    description = "Update Flatpak packages";
+    description = "Update Flatpak packages (system)";
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${pkgs.flatpak}/bin/flatpak update -y";
+    };
+  };
+
+  # Flatpak auto-update (user)
+  systemd.user.services.flatpak-update-user = {
+    description = "Update Flatpak packages (user)";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.flatpak}/bin/flatpak update -y --user";
+    };
+  };
+
+  systemd.user.timers.flatpak-update-user = {
+    description = "Auto-update Flatpak packages (user)";
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "1h";
     };
   };
 
