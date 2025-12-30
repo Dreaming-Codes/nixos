e7c1dc64e3e2c5520a493689e21e41b3897a5cc0
DreamingCodes <me@dreaming.codes>
2025-12-30 02:12:48 -0800

    initial razder integration

--- modules/desktop/sddm/default.nix
+++ modules/desktop/sddm/default.nix
@@ -1,21 +1,19 @@
-{
-  pkgs,
-  ...
-}: let
+{pkgs, ...}: let
   # Custom SDDM theme based on Breeze with per-user session support
-  breezePerUserTheme = pkgs.runCommand "sddm-breeze-per-user" {
-    breezeTheme = "${pkgs.kdePackages.plasma-desktop}/share/sddm/themes/breeze";
-  } ''
-    mkdir -p $out
-    
-    # Copy all files from original breeze theme (--no-preserve to make writable)
-    cp -r --no-preserve=mode,ownership $breezeTheme/* $out/
-    
-    # Override with our modified files
-    cp ${./theme/Main.qml} $out/Main.qml
-    cp ${./theme/SessionButton.qml} $out/SessionButton.qml
-    cp ${./theme/metadata.desktop} $out/metadata.desktop
-  '';
+  breezePerUserTheme =
+    pkgs.runCommand "sddm-breeze-per-user" {
+      breezeTheme = "${pkgs.kdePackages.plasma-desktop}/share/sddm/themes/breeze";
+    } ''
+      mkdir -p $out
+
+      # Copy all files from original breeze theme (--no-preserve to make writable)
+      cp -r --no-preserve=mode,ownership $breezeTheme/* $out/
+
+      # Override with our modified files
+      cp ${./theme/Main.qml} $out/Main.qml
+      cp ${./theme/SessionButton.qml} $out/SessionButton.qml
+      cp ${./theme/metadata.desktop} $out/metadata.desktop
+    '';
 in {
   # Set theme to the path of our custom theme
   services.displayManager.sddm.theme = "${breezePerUserTheme}";
