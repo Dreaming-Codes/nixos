6b61eedaf57cac3dff00c488c6ea359a763a950e
DreamingCodes <me@dreaming.codes>
2026-01-19 17:10:59 -0800

    chore: update DNS settings

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -11,9 +11,12 @@
   services.resolved = {
     enable = true;
     dnsovertls = "true";
-    extraConfig = ''
-      DNS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
-    '';
+    settings.DNS = [
+      "1.1.1.1#cloudflare-dns.com"
+      "1.0.0.1#cloudflare-dns.com"
+      "2606:4700:4700::1111#cloudflare-dns.com"
+      "2606:4700:4700::1001#cloudflare-dns.com"
+    ];
   };
   networking = {
     useDHCP = false;
