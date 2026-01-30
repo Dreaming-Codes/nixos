17dc7a6eea089b041baf81169b303bc195148867
DreamingCodes <me@dreaming.codes>
2026-01-29 19:48:21 -0800

    fix: add sytstem wide mimes

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -1,16 +1,34 @@
 {pkgs, ...}: {
+  # System-wide MIME associations to ensure consistency with portal usage
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "helium.desktop";
+      "x-scheme-handler/http" = "helium.desktop";
+      "x-scheme-handler/https" = "helium.desktop";
+      "text/html" = "helium.desktop";
+    };
+  };
+
   xdg.portal = {
     xdgOpenUsePortal = true;
     enable = true;
 
     config = {
       hyprland = {
-        default = ["hyprland" "gtk" "kde"];
+        default = [
+          "hyprland"
+          "gtk"
+          "kde"
+        ];
         "org.freedesktop.impl.portal.FileChooser" = "kde";
         "org.freedesktop.impl.portal.OpenURI" = "kde";
       };
       plasma = {
-        default = ["kde" "gtk"];
+        default = [
+          "kde"
+          "gtk"
+        ];
         "org.freedesktop.impl.portal.FileChooser" = "kde";
         "org.freedesktop.impl.portal.OpenURI" = "kde";
       };
