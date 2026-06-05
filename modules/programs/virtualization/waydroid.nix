f5537efbfbb8694f4706a92fc1263d6477c3f666
DreamingCodes <me@dreaming.codes>
2026-06-04 23:03:38 -0700

    fix waydroid freeze

--- modules/programs/virtualization/waydroid.nix
+++ modules/programs/virtualization/waydroid.nix
@@ -1,3 +1,23 @@
-{...}: {
+{
+  pkgs,
+  ...
+}: {
   virtualisation.waydroid.enable = true;
+
+  systemd.services.waydroid-container = {
+    path = [pkgs.lxc];
+    preStart = ''
+      cfg=/var/lib/waydroid/waydroid.cfg
+      if [ -f "$cfg" ]; then
+        if ${pkgs.gnugrep}/bin/grep -q '^suspend_action = ' "$cfg"; then
+          ${pkgs.gnused}/bin/sed -i 's/^suspend_action = .*/suspend_action = freeze/' "$cfg"
+        else
+          ${pkgs.gnused}/bin/sed -i '/^\[waydroid\]/a suspend_action = freeze' "$cfg"
+        fi
+      fi
+    '';
+    serviceConfig.Delegate = true;
+  };
+
+  systemd.services.bpftune.serviceConfig.PrivateMounts = true;
 }
