aa1872ef9b730fcdf974d6ff369d6df858373fcf
DreamingCodes <me@dreaming.codes>
2026-06-07 18:56:22 -0700

    optimize steam settings

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,14 +1,25 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
+    gamescopeSession = {
+      enable = true;
+      steamArgs = [
+        "-steamdeck"
+      ];
+      args = [
+        "--mangoapp"
+      ];
+    };
+    extraPackages = with pkgs; [
+      gamescope
+    ];
+    extest.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   environment.systemPackages = with pkgs; [steam-run];
