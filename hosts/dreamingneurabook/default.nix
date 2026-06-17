51c46d3062f591caaea270ca068a843432e0ba79
asahi-install <install@local>
2026-06-17 11:23:06 -0700

    natural scroll on asahi

--- hosts/dreamingneurabook/default.nix
+++ hosts/dreamingneurabook/default.nix
@@ -1,4 +1,10 @@
 {...}: {
   # Machine-specific darwin settings for DreamingNeuraBook go here.
   # The hostName is injected by mkDarwin.
+  system.defaults.trackpad = {
+    ActuateDetents = true;
+    ActuationStrength = 1;
+    FirstClickThreshold = 2;
+    SecondClickThreshold = 2;
+  };
 }
