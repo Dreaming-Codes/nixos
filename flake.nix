5c5fc2145b6471ca0852b38ba94985d1ac4ae87f
asahi-install <install@local>
2026-06-27 14:50:44 -0700

    neuralink cache

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   # compute it from JSON or another file, so the literal lives here.
   nixConfig = {
     substituters = [
+      "https://artifact-s3-gateway.int.n7k.io/n7k-nix-cache?priority=10"
       "https://install.determinate.systems?priority=20"
       "https://cache.nixos.org?priority=40"
       "https://nix-community.cachix.org?priority=41"
@@ -15,6 +16,7 @@
       "https://cache.nixos-cuda.org?priority=45"
     ];
     extra-trusted-public-keys = [
+      "nix-cache.infra.n7k.io-0:WyML6bRQeGqxs/1iSoQrlMuooDlxG15rgjuo7Elmpf4="
       "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
