97258352c518ae555b910442c24b14880a52945c
asahi-install <install@local>
2026-06-29 12:27:31 -0700

    move ca cert to work profile

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -9,5 +9,13 @@ in {
 
   config = lib.mkIf cfg.enable {
     home-manager.users.dreamingcodes.imports = [../../home/work.nix];
+
+    # Neuralink Internal Root CA. The cert lives in this repo under
+    # secrets/certs/ encrypted at rest with git-crypt; the working tree holds
+    # the decrypted PEM, so security.pki bakes it into the system CA bundle at
+    # build time (no --impure needed).
+    security.pki.certificateFiles = [
+      ../../secrets/certs/neuralink-internal-root-ca.crt
+    ];
   };
 }
