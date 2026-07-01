c0cc3d4a2ad8bc1483e9e687d125c05727187b56
asahi-install <install@local>
2026-06-30 21:15:54 -0700

    add bash to envfs fallbacks

--- modules/optimization/envfs.nix
+++ modules/optimization/envfs.nix
@@ -1,12 +1,19 @@
 {
   lib,
   config,
+  pkgs
   ...
 }: let
   cfg = config.dreaming.optimization;
 in {
   config = lib.mkIf cfg.envfs.enable {
-    services.envfs.enable = true;
+    services.envfs = {
+      enable = true;
+      extraFallbackPathCommands = ''
+        ln -s ${pkgs.bash}/bin/bash $out/bash
+      '';
+    };
+
     # Fix FUSE race condition where mount.envfs returns before the kernel
     # registers the mount, causing systemd to report "Failed to mount /usr/bin"
     # and the subsequent dep /bin
