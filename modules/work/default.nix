b06b2e5da1bd437f0d52de319f785d58ca4ad00d
asahi-install <install@local>
2026-07-05 22:08:37 -0700

    add xai api key to envs

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -17,5 +17,23 @@ in {
     security.pki.certificateFiles = [
       ../../secrets/certs/neuralink-internal-root-ca.crt
     ];
+
+    # Work-only secrets (from secrets/secrets.yaml)
+    sops.secrets.xai_work_api_key = {
+      owner = "dreamingcodes";
+      group = "users";
+      mode = "0400";
+    };
+
+    # systemd user sessions load ~/.config/environment.d/*.conf
+    sops.templates."90-xai.conf" = {
+      content = ''
+        XAI_API_KEY=${config.sops.placeholder.xai_work_api_key}
+      '';
+      path = "/home/dreamingcodes/.config/environment.d/90-xai.conf";
+      owner = "dreamingcodes";
+      group = "users";
+      mode = "0400";
+    };
   };
 }
