aac20ddab624fd5b4b4b30ff576fcf2083e10fea
DreamingCodes <me@dreaming.codes>
2026-07-17 20:05:25 -0700

    drop openlogi and add keychron udev

--- flake-modules/packages.nix
+++ flake-modules/packages.nix
@@ -1,11 +1,9 @@
 {...}: {
   perSystem = {pkgs, ...}: {
     packages.nix-file-overlay = pkgs.callPackage ../packages/nix-file-overlay {};
-    packages.openlogi = pkgs.callPackage ../packages/openlogi {};
   };
 
   flake.overlays.default = final: _prev: {
     nix-file-overlay = final.callPackage ../packages/nix-file-overlay {};
-    openlogi = final.callPackage ../packages/openlogi {};
   };
 }
