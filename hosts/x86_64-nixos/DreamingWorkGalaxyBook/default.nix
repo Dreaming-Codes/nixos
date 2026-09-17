dc7611d16fe2558a416fab123f711be750dcd3a9
DreamingCodes <me@dreaming.codes>
2026-09-16 21:19:07 -0700

    fix efi config

--- hosts/x86_64-nixos/DreamingWorkGalaxyBook/default.nix
+++ hosts/x86_64-nixos/DreamingWorkGalaxyBook/default.nix
@@ -16,6 +16,9 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  boot.loader.limine.efiInstallAsRemovable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+
   # Intel Arc B390 iGPU only, no dGPU.
   hardware.graphics.enable = true;
 
